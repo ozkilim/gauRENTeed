5d1aadbbe9424be7caf132cb3c36919c2ab4d094
Oz Kilim <ozkilim@hotmail.co.uk>
2021-06-29 12:13:51 +0200

    first commit

--- Procfile
+++
@@ -1,3 +0,0 @@
-
-web: gunicorn gauRENTeed.wsgi --log-file -
-
--- beta/static/propertySearch.js
+++
@@ -1,6 +0,0 @@
-function initialize() {
-    var input = document.getElementById('searchTextField');
-    new google.maps.places.Autocomplete(input);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
